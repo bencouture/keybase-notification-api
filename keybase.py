6ba6771185f9889c6afcfbc1b9263fe74155ee32
Ben Couture <bencouture@protonmail.com>
2018-07-05 18:17:04 -0400

    Add Sonarr endpoint

--- keybase.py
+++ keybase.py
@@ -23,5 +23,17 @@ def send_message(something):
 	call(["keybase", "chat", "send", username, message])
 	return 'OK'
 
+@app.route("/sonarr", methods=["POST"])
+def send_sonarr():
+	username = os.environ["KEYBASE_USERNAME"]
+	series = request.json['Series']['Title']
+	seasonNumber = str(request.json['Episodes'][0]['SeasonNumber']) 
+	episodeNumber = str(request.json['Episodes'][0]['EpisodeNumber']) 
+	episodeTitle = request.json['Episodes'][0]['Title']
+	message = series + " s" + seasonNumber + "e" + episodeNumber + " - " + episodeTitle
+	
+	call(["keybase", "chat", "send", username, message])
+	return 'OK'
+
 if __name__ == '__main__':
 	app.run(port=int("4433"),host='0.0.0.0',debug=True)
